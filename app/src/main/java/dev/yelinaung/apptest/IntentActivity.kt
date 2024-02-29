c7fcc4db164639c81dbd3dccce26357769a28214
Ye Lin Aung <yelinaung2359@gmail.com>
2024-02-29 19:37:50 +0700

    Intent Part 2

--- app/src/main/java/dev/yelinaung/apptest/IntentActivity.kt
+++ app/src/main/java/dev/yelinaung/apptest/IntentActivity.kt
@@ -2,12 +2,12 @@ package dev.yelinaung.apptest
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import dev.yelinaung.apptest.broadcast.BroadcastActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import dev.yelinaung.apptest.databinding.ActivityIntentBinding
-import java.util.Date
+
 
 class IntentActivity : BaseActivity<ActivityIntentBinding>() {
 
@@ -29,11 +29,48 @@ class IntentActivity : BaseActivity<ActivityIntentBinding>() {
         return ActivityIntentBinding.inflate(layoutInflater)
     }
 
+    private val fileChooserContract =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let { binding.imageView.setImageURI(it) }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val message = intent.getStringExtra(MESSAGE)
         binding.tvMessage.text = message
+
+        binding.btnOpenLink.setOnClickListener { this.openLink() }
+        binding.btnPhoneCall.setOnClickListener { this.callPhoneNumber() }
+        binding.btnShareContent.setOnClickListener { this.shareContent() }
+        binding.btnOpenMap.setOnClickListener { this.openMap() }
+        binding.btnTakePicture.setOnClickListener { this.takePicture() }
+    }
+
+    private fun openLink() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+        startActivity(intent)
+    }
+
+    private fun callPhoneNumber() {
+        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+959253366392"))
+        startActivity(intent)
+    }
+
+    private fun shareContent() {
+
+    }
+
+    private fun openMap() {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://www.google.com/maps/@12.9594868,100.8906177,16z?entry=ttu")
+        )
+        startActivity(intent)
+    }
+
+    private fun takePicture() {
+        fileChooserContract.launch("image/*")
     }
 
 }
