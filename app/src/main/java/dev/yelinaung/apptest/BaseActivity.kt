2394831d69a6bf0e68de78f81ebb2ba9feba6ecc
Ye Lin Aung <yelinaung2359@gmail.com>
2024-03-29 09:39:41 +0700

    UI Part 8 (Imageview)

--- app/src/main/java/dev/yelinaung/apptest/BaseActivity.kt
+++ app/src/main/java/dev/yelinaung/apptest/BaseActivity.kt
@@ -1,9 +1,7 @@
 package dev.yelinaung.apptest
 
-import android.app.ActivityManager
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.widget.Toast
--- app/src/main/java/dev/yelinaung/apptest/userinterface/BasicViewActivity.kt
+++ app/src/main/java/dev/yelinaung/apptest/userinterface/BasicViewActivity.kt
@@ -3,13 +3,12 @@ package dev.yelinaung.apptest.userinterface
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doBeforeTextChanged
 import androidx.core.widget.doOnTextChanged
@@ -27,6 +26,9 @@ class BasicViewActivity : BaseActivity<ActivityBasicViewBinding>() {
 
     }
 
+    private var imageList = mutableListOf<Drawable?>()
+    private var currentIndex = 0
+
     override val pageTitle: String get() = "Basic Views"
 
     override fun setupViewBinding(layoutInflater: LayoutInflater): ActivityBasicViewBinding {
@@ -37,8 +39,8 @@ class BasicViewActivity : BaseActivity<ActivityBasicViewBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.edtOne.doBeforeTextChanged { text, start, count, after ->  }
-        binding.edtOne.doOnTextChanged { text, start, before, count ->  }
+        binding.edtOne.doBeforeTextChanged { text, start, count, after -> }
+        binding.edtOne.doOnTextChanged { text, start, before, count -> }
         binding.edtOne.addTextChangedListener {
             if (it.toString().count() >= 8) {
                 binding.tvOne.text = it
@@ -52,6 +54,21 @@ class BasicViewActivity : BaseActivity<ActivityBasicViewBinding>() {
             val text = button.text
             val toast = Toast.makeText(this@BasicViewActivity, text, Toast.LENGTH_LONG)
             toast.show()
+        }
+        binding.btnChangePhoto.setOnClickListener { changePhoto() }
+
+        imageList.add(AppCompatResources.getDrawable(this, R.drawable.mario))
+        imageList.add(AppCompatResources.getDrawable(this, R.drawable.main_after))
+        imageList.add(AppCompatResources.getDrawable(this, R.drawable.camera))
+    }
+
+    private fun changePhoto() {
+        val drawable = this.imageList[currentIndex]
+        binding.imageView.setImageDrawable(drawable)
+        currentIndex++
+
+        if (currentIndex >= imageList.size) {
+            currentIndex = 0
         }
     }
 
