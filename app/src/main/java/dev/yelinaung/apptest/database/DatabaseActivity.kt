d8679eee900ed7ea6badabce24d17d0c712af130
Ye Lin Aung <yelinaung2359@gmail.com>
2024-08-05 19:33:50 +0700

    Popup Menu

--- app/src/main/java/dev/yelinaung/apptest/database/DatabaseActivity.kt
+++ app/src/main/java/dev/yelinaung/apptest/database/DatabaseActivity.kt
@@ -5,13 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
-import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import dev.yelinaung.apptest.BaseActivity
+import dev.yelinaung.apptest.R
 import dev.yelinaung.apptest.database.entity.Product
 import dev.yelinaung.apptest.databinding.ActivityDatabaseBinding
 import dev.yelinaung.apptest.databinding.ProductItemBinding
@@ -24,7 +25,7 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
 
     private val db by lazy { (application as MyApp).db }
 
-    private val adapter = MyAdapter(this::onClickProduct)
+    private val adapter = MyAdapter(this::onClickEdit, this::onClickDelete)
 
     companion object {
 
@@ -58,7 +59,10 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
         }
 
         binding.floatingActionButton.setOnClickListener {
-            showDialogFragment(AddProductFragment.getInstance(), animation = ScreenAnimation.ENTER_UP_EXIT_STAY)
+            showDialogFragment(
+                AddProductFragment.getInstance(),
+                animation = ScreenAnimation.ENTER_UP_EXIT_STAY
+            )
         }
     }
 
@@ -73,12 +77,17 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
         }
     }
 
-    private fun onClickProduct(product: Product) {
-        showToast("Clicked ${product.name}")
+    private fun onClickEdit(product: Product) {
+        showToast("Clicked Edit for ${product.name}")
+    }
+
+    private fun onClickDelete(product: Product) {
+        showToast("Clicked Delete for ${product.name}")
     }
 
     private class MyAdapter(
-        private val onClickProduct: (Product) -> Unit
+        private val onClickEdit: (Product) -> Unit,
+        private val onClickDelete: (Product) -> Unit
     ) : RecyclerView.Adapter<MyViewHolder>() {
 
         var items = listOf<Product>()
@@ -86,7 +95,7 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
             return MyViewHolder(
                 ProductItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-                onClickProduct
+                onClickEdit, onClickDelete
             )
         }
 
@@ -101,14 +110,14 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
             holder.setupData(items[position])
-            Log.d("TAG", "onBindViewHolder: $position")
         }
 
     }
 
     private class MyViewHolder(
         val binding: ProductItemBinding,
-        private val onClickProduct: (Product) -> Unit
+        private val onClickEdit: (Product) -> Unit,
+        private val onClickDelete: (Product) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
 
         @SuppressLint("SetTextI18n")
@@ -117,11 +126,25 @@ class DatabaseActivity : BaseActivity<ActivityDatabaseBinding>() {
             binding.tvDesc.text = product.description
             binding.tvPrice.text = "${product.price} MMK"
 
-            itemView.setOnClickListener {
-                onClickProduct.invoke(product)
-            }
             itemView.setOnLongClickListener {
-                // to do
+                val menu = PopupMenu(itemView.context, binding.tvTitle, Gravity.END)
+                menu.inflate(R.menu.product_item_menu)
+                menu.setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.edit -> {
+                            onClickEdit.invoke(product)
+                            true
+                        }
+                        R.id.delete -> {
+                            onClickDelete.invoke(product)
+                            true
+                        }
+                        else -> {
+                            false
+                        }
+                    }
+                }
+                menu.show()
                 true
             }
         }
